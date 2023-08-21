ce732f171124b0bec55fca81b1db81e9df1ea6aa
scramble <98439216+rexcaprarum@users.noreply.github.com>
2023-08-21 18:30:19 -0400

    Update day1.py

--- 2020/day1/day1.py
+++ 2020/day1/day1.py
@@ -1,1 +1,14 @@
 
+f = open("day1_data.txt", 'r')
+data = []
+x = 0
+
+for i in f:
+    data.append(int(i))\
+
+for i in range(0, len(data)-1):
+    for y in data:
+        if y + data [i] == 2020:
+            x = y * data[i]
+
+print(x)
