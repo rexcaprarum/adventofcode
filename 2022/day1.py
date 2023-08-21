9cd90f03df23ca338101bbbd20b69e3aa7be5476
rexcaprarum <98439216+rexcaprarum@users.noreply.github.com>
2023-08-20 21:59:24 -0400

    Create day1.py

---
+++ 2022/day1.py
@@ -0,0 +1,21 @@
+
+d = open("day1_data.txt", "r")
+data_list = []
+biggest = 0
+t = 0
+
+for i in d.readlines():
+    data_list.append(i.strip('\n'))
+    
+for i in range(0, len(data_list)):
+    if not data_list[i] == '':
+        t += int(data_list[i])
+    elif data_list[i] == '':
+        if t > biggest:
+            biggest = t 
+            t = 0
+        else:
+            t = 0
+    
+    
+    print(biggest)
